d983bf83f429dfc7be8dd2c2620f2e3b3dedd906
xin <2353759490@qq.com>
2023-04-01 23:17:42 +0800

    git-fix 0.2 commit

--- src/main/java/com/xjl/git/gittest.java
+++ src/main/java/com/xjl/git/gittest.java
@@ -6,5 +6,6 @@ public class gittest {
         System.out.println(2);
         System.out.println(3);
         System.out.println("hot-fix");
+        System.out.println("hot-fix-erro merge");
     }
 }
